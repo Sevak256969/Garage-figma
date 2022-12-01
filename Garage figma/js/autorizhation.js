47c0b2ec82a55c646d21ff21b086dc4587e35620
Sevak Movsisyan <sevmovsisian@gmail.com>
2022-12-01 16:30:01 +0400

    updated all the code

--- Garage figma/js/autorizhation.js
+++ Garage figma/js/autorizhation.js
@@ -1,19 +1,6 @@
-// const form = document.form 
-// function sendRequest(method,url) {
-//     fetch('https://api-generator.retool.com/78mo0Y/registor')
-//     .then(response => response.json())
-//     .then(user => user.filter(user =>user.email === form[0].value))
-//     .then(response => localStorage.setItem('user',JSON.stringify(response)))
-//     // if(localStorage.hasOwnProperty('user')) {
-//     //     window.location.pathname = 
-//     // }  
-// }
-// form.addEventListener('submit',event => event.preventDefault())
-// form[2].addEventListener('click',sendRequest)
-
 const form = document.form 
 function sendRequest() {
-    fetch('https://api-generator.retool.com/78mo0Y/registor')
+    fetch('https://api-generator.retool.com/G0TfpI/registor')
     .then(response => response.json())
     .then(user => user.filter(user =>user.email === form[0].value))
     .then(response => {
@@ -26,16 +13,3 @@ function sendRequest() {
 }
 form.addEventListener('submit',event => event.preventDefault())
 form[2].addEventListener('click',sendRequest)
-
-// const form = document.form;
-// function sendRequest() {
-//   fetch("https://api-generator.retool.com/78mo0Y/registor")
-//     .then((response) =>console.log(response.json()))
-//     .then((user) => user.filter((user) => user.email === form[0].value))
-//     .then((response) =>
-//     console.log(response),
-//       localStorage.setItem("user", JSON.stringify(response[0]))
-//     );
-// }
-// form.addEventListener("submit", (event) => event.preventDefault());
-// form[2].addEventListener("click", sendRequest);
--- Garage figma/js/card.js
+++ Garage figma/js/card.js
@@ -1,20 +1,24 @@
 let cardBascket = document.querySelector(".cards");
 let parentBascket = document.querySelector(".parent_bascket");
+let sumBtn = document.querySelector('.sum-btn')
+let arr = [];
+let allSum;
+let sum = 0;
 if (!localStorage.hasOwnProperty("bascket")) {
-  parentBascket.innerHTML = `  <div class="card_main_empty_div">
-<div class="card_main_empty_div1">
+  parentBascket.innerHTML = `<div class="card_main_empty_div">
+  <div class="card_main_empty_div1">
     <h2 class="cmed1h2">Корзина</h2>
-</div>
-<div class="card_main_empty_div2">
+  </div>
+  <div class="card_main_empty_div2">
     <img class="cmed2_img" src="../Icons/empty_card.svg" alt="Empty Card">
-</div>
-<div class="card_main_empty_div3">
+  </div>
+  <div class="card_main_empty_div3">
     <p class="cmed3_p">Пока что здесь ничего нет, перейдите в <br> каталог и добавьте интересующий товар</p>                    
-</div>
-<div class="card_main_empty_div4">
-<button class="cmed4button"><a href="../garage_home.html">Перейти в каталог</a></button>
-</div>
-</div>`;
+  </div>
+  <div class="card_main_empty_div4">
+  <button class="cmed4button"><a href="../garage_home.html">Перейти в каталог</a></button>
+  </div>
+  </div>`;
 } else {
   let dataReceived = JSON.parse(localStorage.getItem("bascket"));
   dataReceived.forEach((e) => {
@@ -22,24 +26,25 @@ if (!localStorage.hasOwnProperty("bascket")) {
       .then((arr) => arr.json())
       .then((car) => {
         let div = document.createElement("div");
-        div.innerHTML = `
-      <div class="product-card">
-<div class="product-img"><img src="${car.img}" alt="" /></div>
-<div class="product-name"><p>${car.brand}</p></div>
-<div class="product-text">
-<p>${car.about}</p>
-</div>
-<div class="product-price">
-<p>${car.price}р.</p>
-<span class="mention">
-<span class="minus"></span>${e.count}<span class="plus"></span></span
->
-<button ><img src="../Icons/icon_shop.svg" alt=""></button>
-</div> `;
-        cardBascket.append(div);
-      });
+        div.innerHTML = `<div class="product-card">
+        <div class="product-img"><img src="${car.img}" alt="" /></div>
+        <div class="product-name"><p>${car.brand}</p></div>
+        <div class="product-text">
+        <p>${car.about}</p>
+        </div>
+        <div class="product-price">
+        <p>${car.price}р.</p>
+        <span class="mention">
+        <span class="minus"></span>${e.count}<span class="plus"></span></span>
+        <button><img src="../Icons/icon_shop.svg" alt="" /></button></div> `;
+        cardBascket.append(div)
+        sum += +car.price;
+        allSum = sum * e.count  
+      })
+       .then(() => {
+        arr.push(allSum) 
+       })
+       .finally(()=>  sumBtn.innerHTML = arr[arr.length-1]
+      )
   });
 }
-let allPrice = document.querySelector('.price')
-
-// console.log(allPrice.innerHTML);
